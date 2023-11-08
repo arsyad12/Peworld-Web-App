9dc6ac909a2fda325ea7378db63caf17b23ed11c
arsyad12 <33723389+arsyad12@users.noreply.github.com>
2023-11-08 09:51:33 +0700

    [ADD] footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,17 +55,14 @@ export default function Home() {
             <h1 className="h1 mb-10">
               Kenapa harus mencari tallent di peworld
             </h1>
-            {[
-              "Smart",
-              "Full Contribution",
-              "Fast Learnner",
-              "Adaptable",
-            ]?.map((item, key) => (
-              <div key={key} className="flex gap-4 mb-6">
-                <img src="/tick 1.png" />
-                <p>{item}</p>
-              </div>
-            ))}
+            {["Smart", "Full Contribution", "Fast Learnner", "Adaptable"]?.map(
+              (item, key) => (
+                <div key={key} className="flex gap-4 mb-6">
+                  <img src="/tick 1.png" />
+                  <p>{item}</p>
+                </div>
+              )
+            )}
           </div>
         </div>
       </div>
@@ -82,7 +79,16 @@ export default function Home() {
             </p>
             <div className="grid grid-cols-2">
               <div>
-                {["Java.", "Kotlin", "PHP", "Javascript"]?.map(
+                {["Java.", "Kotlin", "PHP", "Javascript"]?.map((item, key) => (
+                  <div key={key} className="flex gap-4 mb-6">
+                    <img src="/tick2.png" />
+                    <p>{item}</p>
+                  </div>
+                ))}
+              </div>
+
+              <div>
+                {["Golang.", "C++", "Ruby", "10 Bahasa Lainnya"]?.map(
                   (item, key) => (
                     <div key={key} className="flex gap-4 mb-6">
                       <img src="/tick2.png" />
@@ -90,20 +96,6 @@ export default function Home() {
                     </div>
                   )
                 )}
-              </div>
-
-              <div>
-                {[
-                  "Golang.",
-                  "C++",
-                  "Ruby",
-                  "10 Bahasa Lainnya"
-                ]?.map((item, key) => (
-                  <div key={key} className="flex gap-4 mb-6">
-                    <img src="/tick2.png" />
-                    <p>{item}</p>
-                  </div>
-                ))}
               </div>
             </div>
           </div>
@@ -114,10 +106,42 @@ export default function Home() {
       </div>
       {/*End of Section 3 */}
 
-{/*Section 4 */}
+      {/*Section 4 */}
+      <div className="container grid place-items-center mt-20">
+        <div className="box-cta border-4 grid grid-cols-2 items-center">
+          <div className="px-10 text-cta">
+            <p>Menuju Tak Terbatas dan Melampauinya</p>
+          </div>
+          <div className="grid justify-items-end pr-10">
+            <button className="border-4 p-2 btn-cta">
+              Mulai Dari Sekarang
+            </button>
+          </div>
+        </div>
+      </div>
+      {/*End of Section 4 */}
 
-{/*End of Section 4 */}
+      <div className="footer mt-20 grid grid-cols-1">
+        <div className="px-10 pt-10">
+          <img src="/logo_white.png" alt="logowhite" />
+          <p className="pt-10 p-footer">
+            Jl. Yos Sudarso No.KM. 8, Umban Sari, Kec. Rumbai, Kota Pekanbaru,
+            Riau 28266
+          </p>
+          <hr className="mt-10" />
+        </div>
 
+        <div className=" grid grid-cols-2 mb-20">
+          <div className="px-10 pt-10">
+            <p className="p-footer">2020 Pewworld. All right reserved</p>
+          </div>
+          <div className="flex justify-end gap-10 pr-10 pt-10">
+            <p className="p-footer">Telepon</p>
+            <p className="p-footer">Email</p>
+            
+          </div>
+        </div>
+      </div>
     </>
   );
 }
