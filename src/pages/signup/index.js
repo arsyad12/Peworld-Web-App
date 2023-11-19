ad198c5a2bf5bdc44cf0d9e1bd4cf70107914fc3
arsyad12 <arsyadbaso123@gmail.com>
2023-11-19 14:41:00 +0700

    [ADD] integration API in signup

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,7 +1,47 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable @next/next/no-img-element */
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function signup() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [fullname, setFullname] = React.useState("");
+  const [company, setCompany] = React.useState("");
+  const [job, setJob] = React.useState("");
+  const [phone, setPhone] = React.useState("");
+  const [errMessage,setErrMessage] = React.useState(null)
+
+
+  const registerHandler = () => {
+    axios({
+      method: "post",
+      url: `https://hire-job.onrender.com/v1/auth/register`,
+      data: {
+        email: email,
+        password: password,
+        fullname: fullname,
+        company: company,
+        job_title: job,
+        phone: phone,
+      },
+    })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+
+        const errFullname = err?.response?.data?.messages?.fullname?.message
+        const errEmail = err?.response?.data?.messages?.email?.message
+        const errPassword = err?.response?.data?.messages?.password?.message
+        const errPhone = err?.response?.data?.messages?.phone?.message
+
+        setErrMessage(errFullname??errEmail??errPassword??errPhone)
+
+      })
+    };
+
   return (
     <>
       <div className="grid grid-cols-1 md:grid-cols-2">
@@ -14,13 +54,12 @@ function signup() {
             <div className="absolute p-[20px]">
               <img src="/logo_white.png" alt="img-login" />
             </div>
-            
             <div className="absolute inset-0 flex justify-center items-center text-[27px] md:text-[40px] w-[100%] md:w-[40%]  p-[30px] text-[white] mx-[30px] md:mx-[0px]">
               <p>
-                Temukan developer berbakat & terbaik di berbagai bidang keahlian
+                Temukan developer berbakat & terbaik di berbagai
+                bidang keahlian
               </p>
             </div>
-
             <div className="w-[127%] h-[100vh] md:w-[100vh] md:h-[100vh] ">
               <img
                 className="w-[100vh] h-[100vh] object-cover"
@@ -34,61 +73,38 @@ function signup() {
         {/* section form */}
         <div className="container flex items-center mx-[50px] md:mx-[0px] ">
           <div className="grid w-[570px]">
-            
             <div className="mt-6">
               <p className="text-[30px] mb-[20px]">Halo, Pewpeople</p>
               <p className="text-[14px] text-[#46505C] mb-[20px]">
-                Silahkan masukan username dan kata sandi, atau anda juga bisa
-                registrasi dengan klik Daftar
+                Passtikan data yang digunakan untuk registrasi adalah data yang valid
               </p>
             </div>
 
-            
+          {errMessage ? (
+          <div className="border border-2 border-[#ffc720] bg-[#ffc720] mt-2 mb-2">
+            <p>{errMessage}</p>
+          </div>
+          ) : null}
+
 
             <label for="basic-url" className="form-label text-[12px] py-2">
               Full Name
             </label>
             <input
               type="text"
-              placeholder="Username"
+              placeholder="Fill With Your Fullname"
               className="border border-[#E2E5ED] p-2"
+              onChange={(item) => setFullname(item.target.value)}
             />
 
-               <label for="basic-url" className="form-label text-[12px] py-2">
-              Role
-            </label>
-            <input
-              type="text"
-              placeholder="Username"
-              className="border border-[#E2E5ED] p-2"
-            />
-
-               <label for="basic-url" className="form-label text-[12px] py-2">
-              Skill
-            </label>
-            <input
-              type="text"
-              placeholder="Username"
-              className="border border-[#E2E5ED] p-2"
-            />
-
-              <label for="basic-url" className="form-label text-[12px] py-2">
-              Address
-            </label>
-            <input
-              type="text"
-              placeholder="Username"
-              className="border border-[#E2E5ED] p-2"
-            />
-
-
-               <label for="basic-url" className="form-label text-[12px] py-2">
+            <label for="basic-url" className="form-label text-[12px] py-2">
               Email
             </label>
             <input
               type="text"
-              placeholder="Username"
+              placeholder="Fill With Your Email"
               className="border border-[#E2E5ED] p-2"
+              onChange={(item) => setEmail(item.target.value)}
             />
 
             <label for="basic-url" className="form-label text-[12px] py-2">
@@ -96,24 +112,56 @@ function signup() {
             </label>
             <input
               type="password"
-              placeholder="Password"
+              placeholder="Type Your Password"
               className="border border-[#E2E5ED] p-2"
+              onChange={(item) => setPassword(item.target.value)}
             />
-            
+
+            <label for="basic-url" className="form-label text-[12px] py-2">
+              Job
+            </label>
+            <input
+              type="text"
+              placeholder="Fill With Your Job"
+              className="border border-[#E2E5ED] p-2"
+              onChange={(item) => setJob(item.target.value)}
+            />
+
+            <label for="basic-url" className="form-label text-[12px] py-2">
+              Company
+            </label>
+            <input
+              type="text"
+              placeholder="Fill With Your Company"
+              className="border border-[#E2E5ED] p-2"
+              onChange={(item) => setCompany(item.target.value)}
+            />
+
+            <label for="basic-url" className="form-label text-[12px] py-2">
+              Phone Number
+            </label>
+            <input
+              type="text"
+              placeholder="Fill With Your Phone Number"
+              className="border border-[#E2E5ED] p-2"
+              onChange={(item) => setPhone(item.target.value)}
+            />
 
             <div className="flex justify-end mt-[10px]">
               <p>Lupa kata sandi?</p>
             </div>
 
-            <button className="border border-[#FBB017] bg-[#FBB017] text-[white] p-3 mt-[10px] rounded-[4px]">
+            <button
+              className="border border-[#FBB017] bg-[#FBB017] text-[white] p-3 mt-[10px] rounded-[4px]"
+              onClick={() => registerHandler()}
+            >
               Masuk
             </button>
-            
+
             <div className="flex justify-center gap-2 mt-[10px] mb-6">
               <p>Anda belum punya akun?</p>
               <p className="text-[#FBB017]"> Daftar disini</p>
             </div>
-            
           </div>
         </div>
       </div>
